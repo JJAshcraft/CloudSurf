d3af80a6b4b05fa576a9c08b3a12826c9d59acf8
JJ Ashcraft <jj@jjashcraft.com>
2018-07-02 12:22:03 -0400

    added Privacy Policy and Terms of service components, removed img from app.js

--- cloudsurf/src/App.js
+++ cloudsurf/src/App.js
@@ -5,12 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to CloudSurf</h1>
-        </header>
-        <p className="App-intro">
-        </p>
+
+
+        
+      
+   
       </div>
     );
   }
