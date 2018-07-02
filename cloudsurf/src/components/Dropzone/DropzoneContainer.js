0ed3779ab0831f99f522a59f5566533f48ec70fa
Allen Hai <allenhai03@gmail.com>
2018-07-02 12:03:49 -0500

    create subcomponents for DropzoneContainer

---
+++ cloudsurf/src/components/Dropzone/DropzoneContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DropzoneImg from './DropzoneImg'
+import DropzoneInfo from './DropzoneInfo'
+import DropzoneUsers from './DropzoneUsers'
+import DropzoneEvents from './DropzoneEvents'
+
+const DropzoneContainer = () =>
+  <React.Fragment>
+    <DropzoneImg />
+    <DropzoneInfo />
+    <DropzoneUsers />
+    <DropzoneEvents />
+  </React.Fragment>
+
+export DropzoneContainer
