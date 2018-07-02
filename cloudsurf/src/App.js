93e7c35872fe4acb44b894b29821b5a1b44c05b7
JJ Ashcraft <jj@jjashcraft.com>
2018-07-02 14:50:08 -0400

    login facebook auth in progress

--- cloudsurf/src/App.js
+++ cloudsurf/src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
 import './App.css';
+import firebase from '../firebase';
+import FrontPage from './components/FrontPage';
+
+const provider = new firebase.auth.FacebookAuthProvider();
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      currentUser: null,
+      isLoggedIn: true,
+      
+    }
+  }
+
+
+FacebookSignIn = () => {
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}  
   render() {
     return (
-      <div className="App">
+    
+      <div>
+       {this.state.isLoggedIn ===true ? <div> <button onClick = {this.FacebookSignIn}>login</button>  <Map /></div>: 
+       
+       <div></div> }
 
-       <Map />
-      </div>
+    </div>
     );
   }
 }
