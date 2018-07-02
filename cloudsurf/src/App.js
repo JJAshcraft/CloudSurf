9e0c6db1ccc903ba1e6093eb50ba3b875fb18af8
sibhat <sibhat.temelso@gmail.com>
2018-07-02 09:32:49 -0700

    map is displaying

--- cloudsurf/src/App.js
+++ cloudsurf/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import Map from './components/Map';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to CloudSurf</h1>
         </header>
         <p className="App-intro">
-        </p>
+        </p> */}
+        <Map />
       </div>
     );
   }
---
+++ cloudsurf/src/components/Map.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
+
+
+export class MapContainer extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const style = {
+            width: '100%',
+            height: '100%'
+          }
+        return(
+            <div className="map"> 
+                <Map google={this.props.google} zoom={5}
+                    style={style}
+                    initialCenter={{
+                        lat: 36.850033,
+                        lng: -87.6500523
+                    }}
+                >
+                    <Marker onClick={() => (console.log('clicked'))}
+                    name={'Current location'}
+                    position={{lat: 37.778519, lng: -122.405640}}
+                    />
+                    
+                </Map>
+            </div>
+        )
+    }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyC7BNr_kr7G686S432xcRQVjtaZzdOUPGs')
+})(MapContainer)
